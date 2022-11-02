50cc9a5e4cbe1c7f629dc060c930e5d1f07eb24d
connectN9iot <115965409+connectN9iot@users.noreply.github.com>
2022-11-02 14:58:39 +0700

    upload

--- index.js
+++ index.js
@@ -1,15 +1,34 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "https://console.firebase.google.com/u/0/project/thanhsc1-fdbf4/database/thanhsc1-fdbf4-default-rtdb/data/~2F"
-//"https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
 
-import { getDatabase, ref, set, child, get } from "https://console.firebase.google.com/u/0/project/thanhsc1-fdbf4/database/thanhsc1-fdbf4-default-rtdb/data/~2F"
-//"https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
+import { getDatabase, ref, set, child, get } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-database.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  https://console.firebase.google.com/u/1/project/connectn9iot/database/connectn9iot-default-rtdb/data/~2F
+  / Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlaXQI3wFfTKB5uRwYhJOysW6W0nbR_TQ",
+  authDomain: "thanhsc1-fdbf4.firebaseapp.com",
+  databaseURL: "https://thanhsc1-fdbf4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "thanhsc1-fdbf4",
+  storageBucket: "thanhsc1-fdbf4.appspot.com",
+  messagingSenderId: "926912176291",
+  appId: "1:926912176291:web:79af1f9b7de1e07692df16",
+  measurementId: "G-BH9Y18PSM3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
   //Điền thông tin Firebase config của cá nhân vào đây
   
 };
